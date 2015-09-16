066d5cc8305774992f2768bb8db61862c2c88619
pcrane <pcrane@goscoutgo.com>
2015-09-16 17:12:34 -0400

    Fixed react tabs. Job click now works as expected.

--- static/js/react_app.js
+++ static/js/react_app.js
@@ -1,23 +1,60 @@
-
-var CandidateForm = React.createClass({
+var CandidateAffix = React.createClass({
   render: function() {
     return (
-      <form name='submit_form' enctype='multipart/form-data'>
+      <div className='col-md-offset-4 col-md-4 affix' data-spy="affix">
+        <JobDetail current_job={this.props.current_job}/>
+        <CandidateForm current_job={this.props.current_job} />
+      </div>
+    );
+  }
+});
+
+var JobDetail = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h4>{this.props.current_job.title}</h4>
+        <div className="panel panel-default">
+            <div className="panel-heading">
+              <h3 className="panel-title">Job Description</h3>
+            </div>
+          <div className="panel-body" style={{height: '200px', overflow: 'scroll'}}>
+            {this.props.current_job.description}
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+var CandidateForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    //var text = React.findDOMNode(this.refs.req_search).value.trim();
+    //console.info('search for', text);
+    //this.props.onJobSearchSubmit({title: text});
+    return;
+  },
+  render: function() {
+    return (
+      <form name='submit_form' encType='multipart/form-data'>
+        <input type="hidden" className="form-control" id="candidate_name" placeholder="Name"
+          value={this.props.current_job.id} required></input>
         <div className="form-group">
-          <label for="candidate_name">Candidate Name</label>
+          <label htmlFor="candidate_name">Candidate Name</label>
           <input type="text" className="form-control" id="candidate_name" placeholder="Name" required></input>
         </div>
         <div className="form-group">
-          <label for="candidate_email">Candidate Email</label>
+          <label htmlFor="candidate_email">Candidate Email</label>
           <input type="email" className="form-control" id="candidate_email" placeholder="your@email.com" required></input>
         </div>
         <div className="form-group">
-          <label for="candidate_resume">Candidate Resume</label>
+          <label htmlFor="candidate_resume">Candidate Resume</label>
           <input type="file" required></input>
           <p className="help-block">Upload the resume to use for this submission.</p>
         </div>
-        <button type="submit" class="btn btn-default" onSubmit="console.log('submitted')">
-          Send
+        <button type="submit" className="btn btn-default" onSubmit={this.handleSubmit}>
+          Submit
         </button>
       </form>
     );
@@ -25,9 +62,12 @@ var CandidateForm = React.createClass({
 });
 
 var Job = React.createClass({
+  jobClicked: function(e) {
+    this.props.onJobClick(this.props.data);
+  },
   render: function() {
     return (
-      <button data-job-id={this.props.data.id} type="button" className="list-group-item">
+      <button data-job-id={this.props.data.id} type="button" className="list-group-item" onClick={this.jobClicked}>
         <h4 className="list-group-item-heading">{this.props.data.title}</h4>
         <div className='row'>
           <label className="col-md-2">Salary</label>
@@ -47,10 +87,14 @@ var Job = React.createClass({
 });
 
 var JobList = React.createClass({
+  onJobClicked: function(job) {
+    this.props.onJobClicked(job);
+  },
   render: function() {
+    var job_click = this.onJobClicked;
     var jobNodes = this.props.data.map(function (job) {
       return (
-        <Job key={job.id} data={job} />
+        <Job data={job} onJobClick={job_click} />
       );
     });
     return (
@@ -74,7 +118,8 @@ var JobForm = React.createClass({
       <form className="form-inline" onSubmit={this.handleSubmit}>
         <div className="form-group">
           <label htmlFor="req_search">Job Search</label>
-          <input type="text" className="form-control" ref="req_search" id="req_search" placeholder="Software Engineer"></input>
+          <input type="text" className="form-control" ref="req_search" id="req_search"
+            placeholder="Software Engineer"></input>
         </div>
         <button type="submit" className="btn btn-default">Search</button>
       </form>
@@ -95,13 +140,16 @@ var JobBox = React.createClass({
       dataType: 'json',
       cache: false,
       success: function(data) {
-        console.log(data);
+        //console.log(data);
         this.setState({data: data.data});
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
+  },
+  handleJobClick: function(job) {
+    this.props.onJobClicked(job);
   },
   handleJobSearchSubmit: function(data) {
     // TODO: submit to the server and refresh the list
@@ -121,15 +169,44 @@ var JobBox = React.createClass({
   },
   render: function() {
     return (
-      <div className="jobBox">
+      <div className="jobBox col-md-6">
         <JobForm onJobSearchSubmit={this.handleJobSearchSubmit} />
-        <JobList data={this.state.data} />
+        <JobList data={this.state.data} onJobClicked={this.handleJobClick}/>
       </div>
     );
   }
 });
 
+var SearchPane = React.createClass({
+  getInitialState: function() {
+    return {current_job: {
+        title:'Select a Job',
+        description: 'Description will show up here'
+    }};
+  },
+  handleJobClick: function(job) {
+    this.setState({current_job: job});
+  },
+  render: function() {
+    return (
+      <div role="tabpanel" className="tab-pane active" id="search">
+        <CandidateAffix current_job={this.state.current_job} />
+        <JobBox url="/search" onJobClicked={this.handleJobClick} />
+      </div>
+    );
+  }
+});
 React.render(
-  <JobBox url="/search"  />,
-  document.getElementById('content')
+  <SearchPane />,
+  document.getElementById('tab_content_anchor')
 );
+
+/*React.render(
+  <JobBox url="/search"  />,
+  document.getElementById('job_box')
+);
+React.render(
+  <CandidateAffix />,
+  document.getElementById('candidate_affix')
+);
+//*/
